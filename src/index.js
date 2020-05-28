e11f2bd04089873bc01f42cc850ccb8655461c36
badjfas <bjwkor@gmail.com>
2020-05-28 22:45:10 +0900

    1.4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import {createStore} from "redux"; 
+import { createStore } from "redux";
 
-const plus = document.getElementById("add")
-const minus = document.getElementById("minus")
-const number = document.querySelector("span")
+const plus = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-const countModifier = (count=0) => {
-  return count;
+const ADD = "add";
+const MINUS = "minus"
 
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 const countStore = createStore(countModifier);
 
-console.log(countStore.getState())
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+plus.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
+
+console.log(countStore.getState());
